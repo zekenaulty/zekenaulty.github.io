2a2572a5a3aba81a49e4ed8701d53e483f7f992d
zekenaulty@gmail.com <zekenaulty@gmail.com>
2023-08-16 21:54:43 -0500

    ... tweaks

--- content/resume.js
+++ content/resume.js
@@ -2,12 +2,32 @@ import { DOM } from '../core/dom.js';
 
 class Resume extends HTMLElement {
 
-  constructor(){
+  constructor() {
     super();
-    
+
+
+  }
+
+  connectedCallback() {
     let vm = this;
+
+    DOM.p('my resume', vm, 'h2');
+
+    let resumeUrl = vm.getAttribute('data-resume');
+
+    if (resumeUel) {
+      fetch(resumeUrl)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          vm.#addResume(data);
+        });
+    }
+  }
+  
+  #addResume(data) {
     
-    DOM.p('my resume', vm._shadow, 'h2');
   }
 }
 
@@ -19,4 +39,3 @@ $router.add({
 });
 
 customElements.define('resume-view', Resume);
-
