ccc81942d50ce7d8800b2d4c35dfdcedf0b18169
Zeke <zekenaulty>
2023-08-29 12:16:29 -0500

    reformat

--- src/background/Background.js
+++ src/background/Background.js
@@ -2,11 +2,8 @@ import React from 'react';
 import './Background.css';
 
 const Background = () => {
-
     return (
-        <section className="bg d-flex flex-column justify-content-center align-items-center">
-            &nbsp;
-        </section>
+        <section className="bg d-flex flex-column justify-content-center align-items-center"></section>
     );
 };
 
