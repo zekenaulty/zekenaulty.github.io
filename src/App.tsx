303848ce40a97bde98d836f1c6ae8b7e6ec03be6
Zeke <zekenaulty>
2024-02-03 18:54:24 -0600

    rip copy pasta

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
 
   return (
-      <div className=".abs-x0-y0 fill-vp bg-layer-0">
+      <div className="abs-x0-y0 fill-vp bg-layer-0">
 
       </div>
   );
