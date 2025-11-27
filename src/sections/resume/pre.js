f40c549740dd860e40fdd22b3c47513edddeec4c
Zeke <zekenaulty@msn.com>
2025-11-27 16:04:48 -0600

    begining, the damn-it I'll use react you fuckers rewrite

--- src/sections/resume/pre.js
+++
@@ -1,6 +0,0 @@
-import { ResumeData } from './data/index.js';
-const resume = DOM.element('pre', {
-    parent: null,
-    text: JSON.stringify(ResumeData, null, 4),
-    classes: ['w-100', 'h-100', 'overflow-auto']
-});
